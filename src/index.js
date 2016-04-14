036634fbf96e8431d7e778e89fbc4d7c3c66ace5
Jon Deibel <jon_deibel@intuit.com>
2016-04-14 14:57:40 -0400

    Got rid of react-router for now

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { Router, Route, Link, browserHistory } from 'react-router'
-import NoPageFound from './error/NoPageFound';
-
-render((
-   <Router history={browserHistory}>
-       <Route path='*' component={NoPageFound} />
-   </Router>
-), document.getElementsByClassName('todoapp')[0]);
+import ReactDOM from 'react-dom';
+// import { render } from 'react-dom'
+// import { Router, Route, Link, browserHistory } from 'react-router'
+// import NoPageFound from './error/NoPageFound';
+//
+// render((
+//    <Router history={browserHistory}>
+//        <Route path='*' component={NoPageFound} />
+//    </Router>
+// ), document.getElementsByClassName('todoapp')[0]);
