16204f79c83f2492a559671eb870862d8403547c
Jon Deibel <jon_deibel@intuit.com>
2016-04-14 11:19:28 -0400

    Created starting point

--- src/basic/components/hello-world.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class Hello extends React.Component {
-    render() {
-        return (
-            <span>{this.props.text}</span>
-        )
-    }
-}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, Link, browserHistory } from 'react-router'
-import BasicComponent from './basic/basic-component';
-import App from './todo/App';
 import NoPageFound from './error/NoPageFound';
 
 render((
    <Router history={browserHistory}>
-       <Route path='/basic' component={BasicComponent} />
-       <Route path='/todo' component={App} />
        <Route path='*' component={NoPageFound} />
    </Router>
 ), document.getElementsByClassName('todoapp')[0]);
